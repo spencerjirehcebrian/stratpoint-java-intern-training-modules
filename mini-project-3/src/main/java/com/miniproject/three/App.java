5fe7c5a8851016edb363475ba43dd5039a6dfb14
spencerjirehcebrian <spencercebrian123@gmail.com>
2024-06-27 13:30:31 +0800

    added gitignore and initialized mini-project-3

---
+++ mini-project-3/src/main/java/com/miniproject/three/App.java
@@ -0,0 +1,13 @@
+package com.miniproject.three;
+
+/**
+ * Hello world!
+ *
+ */
+public class App 
+{
+    public static void main( String[] args )
+    {
+        System.out.println( "Hello World!" );
+    }
+}
